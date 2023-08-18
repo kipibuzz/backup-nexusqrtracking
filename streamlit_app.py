5af5483819c003785407f14638fe1b4ab956ae82
kipibuzz <142256267+kipibuzz@users.noreply.github.com>
2023-08-18 23:13:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -156,25 +156,8 @@ elif menu_choice == menu_choices["QR Code Scanner"]:
     # QR code scanner page
     st.header('QR Code Scanner')
 
-    cap = cv2.VideoCapture(0)
+    # Display camera feed using Streamlit's webcam API
+    st.video(video_capture_source='camera')
 
-    while True:
-        ret, frame = cap.read()
-
-        if not ret:
-            st.error("Camera capture failed")
-            break
-
-        decoded_objects = decode(frame)
-
-        for obj in decoded_objects:
-            qr_data = obj.data.decode('utf-8')
-            st.write(f"QR Code Data: {qr_data}")
-
-        st.image(frame, channels="BGR", use_column_width=True)
-
-        if st.button("Stop"):
-            break
-
-    cap.release()
+    st.warning("Please grant camera access if prompted by your browser.")
 
