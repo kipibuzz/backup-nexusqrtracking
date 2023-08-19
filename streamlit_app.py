cb54a3b01cb1290b89004d21a9a7981182042261
kipibuzz <142256267+kipibuzz@users.noreply.github.com>
2023-08-19 13:03:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -73,7 +73,7 @@ def generate_and_store_qr_codes():
         )
         
         # Update QR_CODE column with S3 file path
-        s3_file_path = f's3://qrstore/{s3_path}'
+        s3_file_path = f's3://qrstore/{s3_path}' 
         cursor.execute(
             "UPDATE EMP SET QR_CODE = %s WHERE ATTENDEE_ID = %s",
             (s3_file_path, attendee_id)
