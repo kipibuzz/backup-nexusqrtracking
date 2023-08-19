5f119ae8ac6819626fed7a864a1d7079d368987b
kipibuzz <142256267+kipibuzz@users.noreply.github.com>
2023-08-19 16:33:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -173,7 +173,8 @@ if st.button("Generate QR Codes"):
         st.warning("QR codes could not be generated. Please check for any issues.")
 
 # QR code scanner page
-    if menu_choice == menu_choices["QR Code Scanner"]:
+ 
+if menu_choice == menu_choices["QR Code Scanner"]:
     st.header('QR Code Scanner')
 
     image = st.camera_input("Show QR code")
